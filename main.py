7e8ef9bd3750533357b1669bc6e455c02ccfefe7
alormada <adam.rola110@gmail.com>
2025-03-01 19:42:30 +0100

    created screen

---
+++ main.py
@@ -0,0 +1,20 @@
+from turtle import Screen
+
+screen = Screen()
+screen.setup(width=800,height=800)
+screen.bgcolor("black")
+screen.title("The Pong Game")
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()

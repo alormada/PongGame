ef661e07c0db253ca8238b585426e91aa2a053be
alormada <adam.rola110@gmail.com>
2025-03-02 14:08:48 +0100

    optimalized game start procedure, added random ball direction from certain range

--- ball.py
+++ ball.py
@@ -1,6 +1,5 @@
 from turtle import Turtle
 import random
-import time
 
 class Ball(Turtle):
     def __init__(self):
@@ -8,8 +7,16 @@ class Ball(Turtle):
         self.shape("circle")
         self.penup()
         self.color("white")
+        self.game_start()
+
+    def game_start(self):
         self.goto(0, 0)
-        self.setheading(random.randint(0, 359))
+        if_set = False
+        while not if_set:
+            heading_number = random.randint(0, 359)
+            if 0 < heading_number < 45 or 135 < heading_number < 225 or heading_number > 315:
+                if_set = True
+                self.setheading(heading_number)
 
     def move(self):
         self.forward(0.3)
@@ -20,16 +27,16 @@ class Ball(Turtle):
             print(f"ANGLE: {angle}", self.heading())
             if angle <= self.heading() < n_angle:
                 if angle == 0:
-                    self.setheading(n_angle + self.heading() % 90)
+                    self.setheading((n_angle + self.heading() % 90 + random.randint(-10, 10)) % 360)
                     break
                 elif angle == 90:
-                    self.setheading(angle - self.heading() % 90)
+                    self.setheading((angle - self.heading() % 90 + random.randint(-10, 10)) % 360)
                     break
                 elif angle == 180:
-                    self.setheading(n_angle + 90 - (self.heading() % 90))
+                    self.setheading((n_angle + 90 - (self.heading() % 90) + random.randint(-10, 10)) % 360)
                     break
                 else:
-                    self.setheading(angle - self.heading() % 90)
+                    self.setheading((angle - self.heading() % 90 + random.randint(-10, 10)) % 360)
                     break
             else:
                 print("ERROR WALL")
